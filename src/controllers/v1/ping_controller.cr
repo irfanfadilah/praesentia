aaf7302a2b60508d35ef7e34ea3e3987cd5c0273
Nurahmadie <nurahmadie@gmail.com>
2020-12-04 10:04:19 +0700

    kickoff

---
+++ src/controllers/v1/ping_controller.cr
@@ -0,0 +1,7 @@
+class V1::PingController < ApplicationController
+  def index
+    context.response.content_type = "application/json"
+    response = { ping: true }
+    response.to_json
+  end
+end

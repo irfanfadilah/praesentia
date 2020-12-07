8bb2e3fa219c5f247e7d5663e97f7e2ef710bbaa
Irfan Fadilah <hello@irfanfadilah.com>
2020-12-07 08:25:51 +0700

    Add Respawn Action with Spawn

--- src/controllers/v1/slack_controller.cr
+++ src/controllers/v1/slack_controller.cr
@@ -1,48 +1,64 @@
 class V1::SlackController < ApplicationController
   def online
-    Activity.online(params)
-    update_block_instance
+    spawn do
+      Activity.online(params)
+      update_block_instance
+    end
     respond_with(200) { text "" }
   end
 
   def offline
-    Activity.offline(params)
-    update_block_instance
+    spawn do
+      Activity.offline(params)
+      update_block_instance
+    end
     respond_with(200) { text "" }
   end
 
   def away
-    Activity.away(params)
-    update_block_instance
+    spawn do
+      Activity.away(params)
+      update_block_instance
+    end
     respond_with(200) { text "" }
   end
 
   def back
-    Activity.back(params)
-    update_block_instance
+    spawn do
+      Activity.back(params)
+      update_block_instance
+    end
     respond_with(200) { text "" }
   end
 
   def interactivity
-    data = JSON.parse params[:payload]
-    action = data["actions"][0]["action_id"]
+    spawn do
+      data = JSON.parse params[:payload]
+      action = data["actions"][0]["action_id"]
 
-    params[:user_id] = data["user"]["id"].as_s
-    params[:channel_id] = data["channel"]["id"].as_s
-    params[:text] = ""
+      params[:user_id] = data["user"]["id"].as_s
+      params[:channel_id] = data["channel"]["id"].as_s
+      params[:text] = ""
 
-    case
-    when action == "Online"
-      Activity.online(params)
-    when action == "Offline"
-      Activity.offline(params)
-    when action == "Away"
-      Activity.away(params)
-    when action == "Back"
-      Activity.back(params)
+      case
+      when action == "Online"
+        Activity.online(params)
+      when action == "Offline"
+        Activity.offline(params)
+      when action == "Away"
+        Activity.away(params)
+      when action == "Back"
+        Activity.back(params)
+      end
+
+      update_block_instance
     end
 
-    update_block_instance
+    respond_with(200) { text "" }
+  end
+
+  def respawn
+    spawn { respawn_block_instance }
     respond_with(200) { text "" }
   end
 
@@ -50,5 +66,9 @@ class V1::SlackController < ApplicationController
     BlockInstanceUpdateJob
       .new(user_id: params[:user_id], channel_id: params[:channel_id])
       .perform
+  end
+
+  private def respawn_block_instance
+    BlockInstanceRespawnJob.new(channel_id: params[:channel_id]).perform
   end
 end

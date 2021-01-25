8fef679083ad4accb7aced98bf742e534c4fbf43
Irfan Fadilah <hello@irfanfadilah.com>
2021-01-25 08:30:12 +0700

    Move UpdateBlockInstance

--- src/services/activity_update.cr
+++ src/services/activity_update.cr
@@ -2,12 +2,18 @@ class ActivityUpdate
   # should wrap this under transaction :(
   def self.bump(state, params)
     activity = Activity.find_by(user_id: params[:user_id])
+
     if activity
       activity.update!(state: state, comment: params[:text])
     else
-      Activity.create!(state: state, user_id: params[:user_id], comment: params[:text])
+      activity = Activity.create!(state: state, user_id: params[:user_id], comment: params[:text])
     end
-    UserChannel.find_or_create_by(user_id: params[:user_id], channel_id: params[:channel_id])
+
+    if params[:channel_id]
+      UserChannel.find_or_create_by(user_id: params[:user_id], channel_id: params[:channel_id])
+    end
+
+    update_block_instance(activity)
   end
 
   def self.online(params)
@@ -24,5 +30,16 @@ class ActivityUpdate
 
   def self.back(params)
     bump("back", params)
+  end
+
+  private def self.update_block_instance(activity)
+    user_id = (activity.user_id || "")
+    channel_id = (activity.channel_id || "")
+
+    return if user_id == "" || channel_id == ""
+
+    BlockInstanceUpdateJob
+      .new(user_id: user_id, channel_id: channel_id)
+      .perform
   end
 end

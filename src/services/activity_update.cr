5fddf3d2cdf6c70f122698f0891fba23bcfbfc74
Irfan Fadilah <hello@irfanfadilah.com>
2021-02-10 17:00:00 +0700

    Handle Empty Channel

--- src/services/activity_update.cr
+++ src/services/activity_update.cr
@@ -2,16 +2,10 @@ class ActivityUpdate
   # should wrap this under transaction :(
   def self.bump(state, params)
     activity = Activity.find_by(user_id: params[:user_id])
+    activity = update_activity(activity, state, params)
 
-    if activity
-      activity.update!(state: state, comment: params[:text])
-    else
-      activity = Activity.create!(state: state, user_id: params[:user_id], channel_id: params[:channel_id], comment: params[:text])
-    end
-
-    UserChannel.find_or_create_by(user_id: activity.user_id, channel_id: activity.channel_id)
-
-    BlockInstanceUpdateJob.new(user_id: activity.user_id, channel_id: activity.channel_id).perform
+    update_user_channel(activity)
+    update_block_instance(activity)
   end
 
   def self.online(params)
@@ -24,5 +18,30 @@ class ActivityUpdate
 
   def self.away(params)
     bump("away", params)
+  end
+
+  # Private Methods
+
+  private def self.update_activity(activity, state, params)
+    if activity
+      if params[:channel_id] == ""
+        activity.update!(state: state, comment: params[:text])
+      else
+        activity.update!(state: state, comment: params[:text], channel_id: params[:channel_id])
+      end
+    else
+      channel_id = (params[:channel_id] == "" ? "G01G2CXUVS9" : params[:channel_id])
+      activity = Activity.create!(state: state, user_id: params[:user_id], channel_id: channel_id, comment: params[:text])
+    end
+
+    activity
+  end
+
+  private def self.update_user_channel(activity)
+    UserChannel.find_or_create_by(user_id: activity.user_id, channel_id: activity.channel_id)
+  end
+
+  private def self.update_block_instance(activity)
+    BlockInstanceUpdateJob.new(user_id: activity.user_id, channel_id: activity.channel_id).perform
   end
 end
